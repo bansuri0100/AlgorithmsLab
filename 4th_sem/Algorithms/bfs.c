2ff1e711c3e09256916855f369bef96cd0aa138a
Bansuri J Sanganal <bansuri0100@gmail.com>
2019-05-27 10:32:58 +0530

    Update bfs.c

--- 4th_sem/Algorithms/bfs.c
+++ 4th_sem/Algorithms/bfs.c
@@ -1,37 +1,52 @@
- #include<stdio.h>
-int n,i,j,a[20][20],visited[20];
-void dfs(int i)
+#include<stdio.h>
+int n,i,j,f=0,r=-1,a[20][20],q[20],visited[20],vo[20],count=0;
+void bfs(int src)
 {
-	int j;
-	printf("\n%d\n",i+1);
-	visited[i]=1;	
-	for(j=0;j<n;j++)
-		if(a[i][j] && !visited[j])
-			dfs(j);
+	
+	int u,v;
+	count++;
+	visited[src]=count;
+	q[++r]=src;
+	while(f<=r)
+	{
+		u=q[f++];
+		for(v=1;v<=n;v++)
+		{
+			if(a[u][v]&&!visited[v])
+			{
+				q[++r]=v;
+				count++;
+				visited[v]=count;
+			}
+		}
+	}			
 	
 }
 void main()
 {
-	int v;
+	int src;
 	printf("Enter number of vertices:\n");
 	scanf("%d",&n);
-	for(i=0;i<n;i++)
+	for(i=1;i<=n;i++)
 	{
-		
 		visited[i]=0;
 	}
 	printf("Enter the adjacency matrix :\n");
-	for(i=0;i<n;i++)
-		for(j=0;j<n;j++)
+	for(i=1;i<=n;i++)
+		for(j=1;j<=n;j++)
 			scanf("%d",&a[i][j]);
-	printf("BFS order :\n");		
-	dfs(0);
-	for(i=0;i<n;i++)
+	printf("Enter the source vertex:\n");
+	scanf("%d",&src);
+	bfs(src);
+	for(i=1;i<=n;i++)
 	{
 		if(visited[i]==0)
-			dfs(i);
+			bfs(i);
 	}
-	
-	
-}										
-			
+	for(i=1;i<=n;i++)
+		vo[visited[i]]=i;
+		
+	for(i=1;i<=n;i++)
+		printf("%d\n",vo[i]);
+		
+}
