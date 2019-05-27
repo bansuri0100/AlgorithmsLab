2934d07b5fc8c9372501f11af6946ad1e01dbb1c
Bansuri J Sanganal <bansuri0100@gmail.com>
2019-05-27 10:32:11 +0530

    Update bfs.c

--- 4th_sem/Algorithms/bfs.c
+++ 4th_sem/Algorithms/bfs.c
@@ -1,15 +1,14 @@
-#include<stdio.h>
-int n,i,j,f=0,r=-1,a[20][20],q[20],visited[20];
-void bfs(int v)
+ #include<stdio.h>
+int n,i,j,a[20][20],visited[20];
+void dfs(int i)
 {
-	for(i=0;i<n;i++)
-		if(a[v][i] && !visited[i])
-			q[++r]=i;
-	if(f<=r)
-	{
-		visited[q[f]]=1;
-		bfs(q[f++]);
-	}
+	int j;
+	printf("\n%d\n",i+1);
+	visited[i]=1;	
+	for(j=0;j<n;j++)
+		if(a[i][j] && !visited[j])
+			dfs(j);
+	
 }
 void main()
 {
@@ -18,35 +17,21 @@ void main()
 	scanf("%d",&n);
 	for(i=0;i<n;i++)
 	{
-		q[i]=0;
+		
 		visited[i]=0;
 	}
 	printf("Enter the adjacency matrix :\n");
 	for(i=0;i<n;i++)
 		for(j=0;j<n;j++)
 			scanf("%d",&a[i][j]);
-	printf("Enter the source vertex:\n");
-	scanf("%d",&v);
-	bfs(v);
+	printf("BFS order :\n");		
+	dfs(0);
 	for(i=0;i<n;i++)
 	{
 		if(visited[i]==0)
-			bfs(i);
+			dfs(i);
 	}
-	printf("queus:\n");
-	for(i=0;i<n;i++)
-		
-			printf("%d\n",q[i]+1);
-	for(i=0;i<n;i++)
-	{
-		if(visited[i])
-			printf("%d\n",i+1);
-		else
-		{
-			printf("BFS not possible as all nodes are not reachable\n");
-			break;
-		}
-	}
+	
 	
 }										
 			
